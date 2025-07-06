89b308ccc6e0b84191858a81fe13d264e909277b
v.sharma <v.sharma@digitalocean.com>
2025-07-06 18:20:04 +0530

    Comment out MallardDuck example in MiniDuckSimulator and add commands.txt for execution instructions

--- chap0/part1/MiniDuckSimulator.ts
+++ chap0/part1/MiniDuckSimulator.ts
@@ -3,15 +3,16 @@ import { MallardDuck } from "./MallardDuck";
 import { ModelDuck } from "./ModelDuck";
 
 function main(): void {
-  const mallard = new MallardDuck();
-  mallard.display();
-  mallard.performQuack();
-  mallard.performFly();
-  mallard.swim();
+  // const mallard = new MallardDuck();
+  // mallard.display();
+  // mallard.performQuack();
+  // mallard.performFly();
+  // mallard.swim();
 
   //new examole
   const model = new ModelDuck();
   model.performFly();
+  //Dynamically change the fly behaviour
   model.setFlyBehaviour(new FlyRocketPowered());
   model.performFly();
 }
